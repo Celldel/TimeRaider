c104089792e599a42ade0378e9c177a6ed3b6fd1
Celldel <Mario_strand@hotmail.com>
2015-12-14 11:39:01 +0100

    Jag är nöjd
    Trist Mario gör om gör rätt

--- Assets/NyMovement.cs
+++ Assets/NyMovement.cs
@@ -4,12 +4,12 @@ using System.Collections;
 public class NyMovement : MonoBehaviour {
 
 	// Use this for initialization
-	void Start () {
-	
-	}
-	
-	// Update is called once per frame
-	void Update () {
-	
-	}
-}
+//	void Start () {
+//	
+//	}
+//	
+//	// Update is called once per frame
+//	void Update () {
+//	
+//	}
+//}
